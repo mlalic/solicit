e01d0e76af58055057259ddb9b1620946c5d305e
Marko Lalic <marko.lalic@gmail.com>
2015-01-12 02:24:54 +0100

    Implement an integer encoder according to HPACK

--- src/hpack/decoder.rs
+++ src/hpack/decoder.rs
@@ -31,9 +31,36 @@ fn decode_integer(buf: &[u8], prefix_size: u8) -> (usize, usize) {
     (value, total)
 }
 
+/// Encode an integer to the representation defined by HPACK.
+///
+/// Returns a newly allocated `Vec` containing the encoded bytes.
+/// Only `prefix_size` lowest-order bits of the first byte in the
+/// array are guaranteed to be used.
+fn encode_integer(mut value: usize, prefix_size: u8) -> Vec<u8> {
+    let mask: usize = ((1u8 << prefix_size) - 1) as usize;
+    if value < mask {
+        // Right now, the caller would need to be the one to combine
+        // the other part of the prefix byte (but would know that it's
+        // safe to do so using the bit-wise or).
+        return vec![value as u8];
+    }
+
+    let mut res: Vec<u8> = Vec::new();
+    res.push(mask as u8);
+    value -= mask;
+    while value >= 128 {
+        res.push(((value % 128) + 128) as u8);
+        value = value / 128;
+    }
+    res.push(value as u8);
+
+    res
+}
+
 
 mod tests {
     use super::{decode_integer};
+    use super::{encode_integer};
 
     #[test]
     fn test_decode_integer() {
@@ -47,4 +74,10 @@ mod tests {
         assert_eq!((127us, 2), decode_integer(&[127, 0], 7));
     }
 
+    #[test]
+    fn test_encode_integer() {
+        assert_eq!(encode_integer(10, 5), [10]);
+        assert_eq!(encode_integer(1337, 5), [31, 154, 10]);
+        assert_eq!(encode_integer(127, 7), [127, 0]);
+    }
 }
