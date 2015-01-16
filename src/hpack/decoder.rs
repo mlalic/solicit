80ed15b1dc05a63d2d7112a6e8081af4e37acd23
Marko Lalic <marko.lalic@gmail.com>
2015-01-16 22:46:38 +0100

    Move DynamicTable implementation to hpack mod root
    The DynamicTable is also used by the Encoder and, as such,
    should be shared with the Decoder.

--- src/hpack/decoder.rs
+++ src/hpack/decoder.rs
@@ -17,13 +17,12 @@
 ///
 /// assert_eq!([(b":method".to_vec(), b"GET".to_vec())], header_list);
 /// ```
-use std::fmt;
-use std::collections::RingBuf;
 
 use super::huffman::HuffmanDecoder;
 use super::huffman::HuffmanDecoderError;
 
 use super::STATIC_TABLE;
+use super::DynamicTable;
 
 /// Decodes an integer encoded with a given prefix size (in bits).
 /// Assumes that the buffer `buf` contains the integer to be decoded,
@@ -110,149 +109,6 @@ fn encode_integer(mut value: usize, prefix_size: u8) -> Vec<u8> {
     res.push(value as u8);
 
     res
-}
-
-/// A struct representing the dynamic table that needs to be maintained by the
-/// coder.
-///
-/// The dynamic table contains a number of recently used headers. The size of
-/// the table is constrained to a certain number of octets. If on insertion of
-/// a new header into the table, the table would exceed the maximum size,
-/// headers are evicted in a FIFO fashion until there is enough room for the
-/// new header to be inserted. (Therefore, it is possible that though all
-/// elements end up being evicted, there is still not enough space for the new
-/// header: when the size of this individual header exceeds the maximum size of
-/// the table.)
-///
-/// The current size of the table is calculated, based on the IETF definition,
-/// as the sum of sizes of each header stored within the table, where the size
-/// of an individual header is
-/// `len_in_octets(header_name) + len_in_octets(header_value) + 32`.
-///
-/// Note: the maximum size of the dynamic table does not have to be equal to
-/// the maximum header table size as defined by a "higher level" protocol
-/// (such as the `SETTINGS_HEADER_TABLE_SIZE` setting in HTTP/2), since HPACK
-/// can choose to modify the dynamic table size on the fly (as long as it keeps
-/// it below the maximum value set by the protocol). So, the `DynamicTable`
-/// only cares about the maximum size as set by the HPACK {en,de}coder and lets
-/// *it* worry about making certain that the changes are valid according to
-/// the (current) constraints of the protocol.
-struct DynamicTable {
-    table: RingBuf<(Vec<u8>, Vec<u8>)>,
-    size: usize,
-    max_size: usize,
-}
-
-impl DynamicTable {
-    /// Creates a new empty dynamic table with a default size.
-    fn new() -> DynamicTable {
-        // The default maximum size corresponds to the default HTTP/2
-        // setting
-        DynamicTable::with_size(4096)
-    }
-
-    /// Creates a new empty dynamic table with the given maximum size.
-    fn with_size(max_size: usize) -> DynamicTable {
-        DynamicTable {
-            table: RingBuf::new(),
-            size: 0,
-            max_size: max_size,
-        }
-    }
-
-    /// Returns the current size of the table in octets, as defined by the IETF
-    /// HPACK spec.
-    fn get_size(&self) -> usize {
-        self.size
-    }
-
-    /// Sets the new maximum table size.
-    ///
-    /// If the current size of the table is larger than the new maximum size,
-    /// existing headers are evicted in a FIFO fashion until the size drops
-    /// below the new maximum.
-    fn set_max_table_size(&mut self, new_max_size: usize) {
-        self.max_size = new_max_size;
-        // Make the table size fit within the new constraints.
-        self.consolidate_table();
-    }
-
-    /// Returns the maximum size of the table in octets.
-    fn get_max_table_size(&self) -> usize {
-        self.max_size
-    }
-
-    /// Add a new header to the dynamic table.
-    ///
-    /// The table automatically gets resized, if necessary.
-    ///
-    /// Do note that, under the HPACK rules, it is possible the given header
-    /// is not found in the dynamic table after this operation finishes, in
-    /// case the total size of the given header exceeds the maximum size of the
-    /// dynamic table.
-    fn add_header(&mut self, name: Vec<u8>, value: Vec<u8>) {
-        // This is how the HPACK spec makes us calculate the size.  The 32 is
-        // a magic number determined by them (under reasonable assumptions of
-        // how the table is stored).
-        self.size += name.len() + value.len() + 32;
-        debug!("New dynamic table size {}", self.size);
-        // Now add it to the internal buffer
-        self.table.push_front((name, value));
-        // ...and make sure we're not over the maximum size.
-        self.consolidate_table();
-        debug!("After consolidation dynamic table size {}", self.size);
-    }
-
-    /// Consolidates the table entries so that the table size is below the
-    /// maximum allowed size, by evicting headers from the table in a FIFO
-    /// fashion.
-    fn consolidate_table(&mut self) {
-        while self.size > self.max_size {
-            {
-                let last_header = match self.table.back() {
-                    Some(x) => x,
-                    None => {
-                        // Can never happen as the size of the table must reach
-                        // 0 by the time we've exhausted all elements.
-                        // The only time it *could* happen is if max_size were 0
-                        // too.
-                        panic!("Size of table != 0, but no headers left!");
-                    }
-                };
-                self.size -= last_header.0.len() + last_header.1.len() + 32;
-            }
-            self.table.pop_back();
-        }
-    }
-
-    /// Returns the number of headers in the dynamic table.
-    ///
-    /// This is different than the size of the dynamic table.
-    fn len(&self) -> usize {
-        self.table.len()
-    }
-
-    /// Converts the current state of the table to a `Vec`
-    fn get_table_as_list(&self) -> Vec<(Vec<u8>, Vec<u8>)> {
-        let mut ret: Vec<(Vec<u8>, Vec<u8>)> = Vec::new();
-        for elem in self.table.iter() {
-            ret.push(elem.clone());
-        }
-
-        ret
-    }
-
-    /// Returns a reference to the header at the given index, if found in the
-    /// dynamic table.
-    fn get(&self, index: usize) -> Option<&(Vec<u8>, Vec<u8>)> {
-        self.table.get(index)
-    }
-}
-
-impl fmt::Show for DynamicTable {
-    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "{:?}", self.table)
-    }
 }
 
 /// Decodes headers encoded using HPACK.
@@ -580,103 +436,12 @@ impl Decoder {
 mod tests {
     use super::{decode_integer};
     use super::{encode_integer};
-    use super::DynamicTable;
     use super::FieldRepresentation;
     use super::decode_string;
     use super::Decoder;
     use super::{DecoderError, DecoderResult};
     use super::{IntegerDecodingError, StringDecodingError};
     use super::super::huffman::HuffmanDecoderError;
-
-    #[test]
-    fn test_dynamic_table_size_calculation_simple() {
-        let mut table = DynamicTable::new();
-        // Sanity check
-        assert_eq!(0, table.get_size());
-
-        table.add_header(b"a".to_vec(), b"b".to_vec());
-
-        assert_eq!(32 + 2, table.get_size());
-    }
-
-    #[test]
-    fn test_dynamic_table_size_calculation() {
-        let mut table = DynamicTable::new();
-
-        table.add_header(b"a".to_vec(), b"b".to_vec());
-        table.add_header(b"123".to_vec(), b"456".to_vec());
-        table.add_header(b"a".to_vec(), b"b".to_vec());
-
-        assert_eq!(3 * 32 + 2 + 6 + 2, table.get_size());
-    }
-
-    /// Tests that the `DynamicTable` gets correctly resized (by evicting old
-    /// headers) if it exceeds the maximum size on an insertion.
-    #[test]
-    fn test_dynamic_table_auto_resize() {
-        let mut table = DynamicTable::with_size(38);
-        table.add_header(b"a".to_vec(), b"b".to_vec());
-        assert_eq!(32 + 2, table.get_size());
-
-        table.add_header(b"123".to_vec(), b"456".to_vec());
-
-        // Resized?
-        assert_eq!(32 + 6, table.get_size());
-        // Only has the second header?
-        assert_eq!(table.get_table_as_list(), vec![
-            (b"123".to_vec(), b"456".to_vec())]);
-    }
-
-    /// Tests that when inserting a new header whose size is larger than the
-    /// size of the entire table, the table is fully emptied.
-    #[test]
-    fn test_dynamic_table_auto_resize_into_empty() {
-        let mut table = DynamicTable::with_size(38);
-        table.add_header(b"a".to_vec(), b"b".to_vec());
-        assert_eq!(32 + 2, table.get_size());
-
-        table.add_header(b"123".to_vec(), b"4567".to_vec());
-
-        // Resized and empty?
-        assert_eq!(0, table.get_size());
-        assert_eq!(0, table.get_table_as_list().len());
-    }
-
-    /// Tests that when changing the maximum size of the `DynamicTable`, the
-    /// headers are correctly evicted in order to keep its size below the new
-    /// max.
-    #[test]
-    fn test_dynamic_table_change_max_size() {
-        let mut table = DynamicTable::new();
-        table.add_header(b"a".to_vec(), b"b".to_vec());
-        table.add_header(b"123".to_vec(), b"456".to_vec());
-        table.add_header(b"c".to_vec(), b"d".to_vec());
-        assert_eq!(3 * 32 + 2 + 6 + 2, table.get_size());
-
-        table.set_max_table_size(38);
-
-        assert_eq!(32 + 2, table.get_size());
-        assert_eq!(table.get_table_as_list(), vec![
-            (b"c".to_vec(), b"d".to_vec())]);
-    }
-
-    /// Tests that setting the maximum table size to 0 clears the dynamic
-    /// table.
-    #[test]
-    fn test_dynamic_table_clear() {
-        let mut table = DynamicTable::new();
-        table.add_header(b"a".to_vec(), b"b".to_vec());
-        table.add_header(b"123".to_vec(), b"456".to_vec());
-        table.add_header(b"c".to_vec(), b"d".to_vec());
-        assert_eq!(3 * 32 + 2 + 6 + 2, table.get_size());
-
-        table.set_max_table_size(0);
-
-        assert_eq!(0, table.len());
-        assert_eq!(0, table.get_table_as_list().len());
-        assert_eq!(0, table.get_size());
-        assert_eq!(0, table.get_max_table_size());
-    }
 
     /// Tests that valid integer encodings are properly decoded.
     #[test]
