31eecfbef33c38007d676c3e58094e67f3a2a593
Marko Lalic <marko.lalic@gmail.com>
2015-01-16 22:39:40 +0100

    Move the static table to the hpack mod root
    The static table will need to be used by the decoder too, so it
    should be somewhere where both the encoder and the decoder can
    reach it.

--- src/hpack/decoder.rs
+++ src/hpack/decoder.rs
@@ -23,6 +23,8 @@ use std::collections::RingBuf;
 use super::huffman::HuffmanDecoder;
 use super::huffman::HuffmanDecoderError;
 
+use super::STATIC_TABLE;
+
 /// Decodes an integer encoded with a given prefix size (in bits).
 /// Assumes that the buffer `buf` contains the integer to be decoded,
 /// with the first byte representing the octet that contains the
@@ -109,72 +111,6 @@ fn encode_integer(mut value: usize, prefix_size: u8) -> Vec<u8> {
 
     res
 }
-
-/// The table represents the static header table defined by the HPACK spec.
-/// (HPACK, Appendix A)
-static STATIC_TABLE: &'static [(&'static [u8], &'static [u8])] = &[
-  (b":authority", b""),
-  (b":method", b"GET"),
-  (b":method", b"POST"),
-  (b":path", b"/"),
-  (b":path", b"/index.html"),
-  (b":scheme", b"http"),
-  (b":scheme", b"https"),
-  (b":status", b"200"),
-  (b":status", b"204"),
-  (b":status", b"206"),
-  (b":status", b"304"),
-  (b":status", b"400"),
-  (b":status", b"404"),
-  (b":status", b"500"),
-  (b"accept-", b""),
-  (b"accept-encoding", b"gzip, deflate"),
-  (b"accept-language", b""),
-  (b"accept-ranges", b""),
-  (b"accept", b""),
-  (b"access-control-allow-origin", b""),
-  (b"age", b""),
-  (b"allow", b""),
-  (b"authorization", b""),
-  (b"cache-control", b""),
-  (b"content-disposition", b""),
-  (b"content-encoding", b""),
-  (b"content-language", b""),
-  (b"content-length", b""),
-  (b"content-location", b""),
-  (b"content-range", b""),
-  (b"content-type", b""),
-  (b"cookie", b""),
-  (b"date", b""),
-  (b"etag", b""),
-  (b"expect", b""),
-  (b"expires", b""),
-  (b"from", b""),
-  (b"host", b""),
-  (b"if-match", b""),
-  (b"if-modified-since", b""),
-  (b"if-none-match", b""),
-  (b"if-range", b""),
-  (b"if-unmodified-since", b""),
-  (b"last-modified", b""),
-  (b"link", b""),
-  (b"location", b""),
-  (b"max-forwards", b""),
-  (b"proxy-authenticate", b""),
-  (b"proxy-authorization", b""),
-  (b"range", b""),
-  (b"referer", b""),
-  (b"refresh", b""),
-  (b"retry-after", b""),
-  (b"server", b""),
-  (b"set-cookie", b""),
-  (b"strict-transport-security", b""),
-  (b"transfer-encoding", b""),
-  (b"user-agent", b""),
-  (b"vary", b""),
-  (b"via", b""),
-  (b"www-authenticate", b""),
-];
 
 /// A struct representing the dynamic table that needs to be maintained by the
 /// coder.
