f390a3b2d9d76876e9bc9624b1fefcb48d2911a2
Marko Lalic <marko.lalic@gmail.com>
2015-01-16 22:51:36 +0100

    Move `encode_integer` to the encoder module
    Encoding functionality, even though useful for writing decoder tests,
    should belong in the `encoder` module.

--- src/hpack/decoder.rs
+++ src/hpack/decoder.rs
@@ -85,32 +85,6 @@ fn decode_integer(buf: &[u8], prefix_size: u8)
             IntegerDecodingError::NotEnoughOctets))
 }
 
-/// Encode an integer to the representation defined by HPACK.
-///
-/// Returns a newly allocated `Vec` containing the encoded bytes.
-/// Only `prefix_size` lowest-order bits of the first byte in the
-/// array are guaranteed to be used.
-fn encode_integer(mut value: usize, prefix_size: u8) -> Vec<u8> {
-    let mask: usize = ((1u8 << prefix_size) - 1) as usize;
-    if value < mask {
-        // Right now, the caller would need to be the one to combine
-        // the other part of the prefix byte (but would know that it's
-        // safe to do so using the bit-wise or).
-        return vec![value as u8];
-    }
-
-    let mut res: Vec<u8> = Vec::new();
-    res.push(mask as u8);
-    value -= mask;
-    while value >= 128 {
-        res.push(((value % 128) + 128) as u8);
-        value = value / 128;
-    }
-    res.push(value as u8);
-
-    res
-}
-
 /// Decodes headers encoded using HPACK.
 ///
 /// For now, incremental decoding is not supported, i.e. it is necessary
@@ -435,7 +409,7 @@ impl Decoder {
 #[cfg(test)]
 mod tests {
     use super::{decode_integer};
-    use super::{encode_integer};
+    use super::super::encoder::encode_integer;
     use super::FieldRepresentation;
     use super::decode_string;
     use super::Decoder;
@@ -487,13 +461,6 @@ mod tests {
                             decode_integer(&[10], 0));
         assert_integer_err!(IntegerDecodingError::InvalidPrefix,
                             decode_integer(&[10], 9));
-    }
-
-    #[test]
-    fn test_encode_integer() {
-        assert_eq!(encode_integer(10, 5), [10]);
-        assert_eq!(encode_integer(1337, 5), [31, 154, 10]);
-        assert_eq!(encode_integer(127, 7), [127, 0]);
     }
 
     #[test]
