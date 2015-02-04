b38dd50527c1df6fc9ae700f346d777f405a5403
Marko Lalic <marko.lalic@gmail.com>
2015-02-04 21:34:18 +0100

    Implement unpacking HTTP/2 frame headers
    HTTP/2 frame headers are a 9-octet sequence that encodes the frame's
    length, type, flags, and stream_id.  The `unpack_header` function
    takes a 9-octet buffer and returns a 4-tuple representing the
    components of the header.

--- src/http/frame.rs
+++ src/http/frame.rs
@@ -1,1 +1,80 @@
 //! The module contains the implementation of HTTP/2 frames.
+
+/// An alias for the 9-byte buffer that each HTTP/2 frame header must be stored
+/// in.
+type FrameHeaderBuffer = [u8; 9];
+/// An alias for the 4-tuple representing the components of each HTTP/2 frame
+/// header.
+type FrameHeader = (u32, u8, u8, u32);
+
+/// Deconstructs a `FrameHeader` into its corresponding 4 components,
+/// represented as a 4-tuple: `(length, frame_type, flags, stream_id)`.
+///
+/// The frame `type` and `flags` components are returned as their original
+/// octet representation, rather than reinterpreted.
+fn unpack_header(header: &FrameHeaderBuffer) -> FrameHeader {
+    let length: u32 = (
+        ((header[0] as u32) << 16) |
+        ((header[1] as u32) <<  8) |
+        ((header[2] as u32) <<  0));
+    let frame_type = header[3];
+    let flags = header[4];
+    let stream_id: u32 = (
+        ((header[5] as u32) << 24) |
+        ((header[6] as u32) << 16) |
+        ((header[7] as u32) <<  8) |
+        ((header[8] as u32) <<  0));
+
+    (length, frame_type, flags, stream_id)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        unpack_header,
+    };
+
+    /// Tests that the `unpack_header` function correctly returns the
+    /// components of HTTP/2 frame headers.
+    #[test]
+    fn test_unpack_header() {
+        {
+            let header = [0; 9];
+            assert_eq!((0, 0, 0, 0), unpack_header(&header));
+        }
+        {
+            let header = [0, 0, 1, 2, 3, 0, 0, 0, 4];
+            assert_eq!((1, 2, 3, 4), unpack_header(&header));
+        }
+        {
+            let header = [0, 0, 1, 200, 100, 0, 0, 0, 4];
+            assert_eq!((1, 200, 100, 4), unpack_header(&header));
+        }
+        {
+            let header = [0, 0, 1, 0, 0, 0, 0, 0, 0];
+            assert_eq!((1, 0, 0, 0), unpack_header(&header));
+        }
+        {
+            let header = [0, 1, 0, 0, 0, 0, 0, 0, 0];
+            assert_eq!((256, 0, 0, 0), unpack_header(&header));
+        }
+        {
+            let header = [1, 0, 0, 0, 0, 0, 0, 0, 0];
+            assert_eq!((256 * 256, 0, 0, 0), unpack_header(&header));
+        }
+        {
+            let header = [0, 0, 0, 0, 0, 0, 0, 0, 1];
+            assert_eq!((0, 0, 0, 1), unpack_header(&header));
+        }
+        {
+            let header = [0xFF, 0xFF, 0xFF, 0, 0, 0, 0, 0, 1];
+            assert_eq!(((1 << 24) - 1, 0, 0, 1), unpack_header(&header));
+        }
+        {
+            let header = [0xFF, 0xFF, 0xFF, 0, 0, 1, 1, 1, 1];
+            assert_eq!(
+                ((1 << 24) - 1, 0, 0, 1 + (1 << 8) + (1 << 16) + (1 << 24)),
+                unpack_header(&header));
+        }
+    }
+}
