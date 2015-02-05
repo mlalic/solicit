5c3ffa027229f8c0e61e95918c2e462eb555e623
Marko Lalic <marko.lalic@gmail.com>
2015-02-05 15:36:47 +0100

    Implement settings deserialization
    Each HTTP/2 setting can be represented by a 6 byte sequence,
    as per section 6.5.1. of the HTTP/2 spec.

--- src/http/frame.rs
+++ src/http/frame.rs
@@ -78,11 +78,71 @@ pub enum HttpSetting {
     MaxHeaderListSize(u32),
 }
 
+impl HttpSetting {
+    /// Creates a new `HttpSetting` with the correct variant corresponding to
+    /// the given setting id, based on the settings IDs defined in section
+    /// 6.5.2.
+    pub fn from_id(id: u16, val: u32) -> Option<HttpSetting> {
+        match id {
+            1 => Some(HttpSetting::HeaderTableSize(val)),
+            2 => Some(HttpSetting::EnablePush(val)),
+            3 => Some(HttpSetting::MaxConcurrentStreams(val)),
+            4 => Some(HttpSetting::InitialWindowSize(val)),
+            5 => Some(HttpSetting::MaxFrameSize(val)),
+            6 => Some(HttpSetting::MaxHeaderListSize(val)),
+            _ => None,
+        }
+    }
+
+    /// Creates a new `HttpSetting` by parsing the given buffer of 6 bytes,
+    /// which contains the raw byte representation of the setting, according
+    /// to the "SETTINGS format" defined in section 6.5.1.
+    ///
+    /// The `raw_setting` parameter should have length at least 6 bytes, since
+    /// the length of the raw setting is exactly 6 bytes.
+    ///
+    /// # Panics
+    ///
+    /// If given a buffer shorter than 6 bytes, the function will panic.
+    fn parse_setting(raw_setting: &[u8]) -> Option<HttpSetting> {
+        let id: u16 = ((raw_setting[0] as u16) << 8) | (raw_setting[1] as u16);
+        let val: u32 = unpack_octets_4!(raw_setting, 2, u32);
+
+        HttpSetting::from_id(id, val)
+    }
+
+    /// Returns the setting ID as an unsigned 16 bit integer, as defined in
+    /// section 6.5.2.
+    pub fn get_id(&self) -> u16 {
+        match self {
+            &HttpSetting::HeaderTableSize(_) => 1,
+            &HttpSetting::EnablePush(_) => 2,
+            &HttpSetting::MaxConcurrentStreams(_) => 3,
+            &HttpSetting::InitialWindowSize(_) => 4,
+            &HttpSetting::MaxFrameSize(_) => 5,
+            &HttpSetting::MaxHeaderListSize(_) => 6,
+        }
+    }
+
+    /// Gets the setting value by unpacking it from the wrapped `u32`.
+    pub fn get_val(&self) -> u32 {
+        match self {
+            &HttpSetting::HeaderTableSize(ref val) => val.clone(),
+            &HttpSetting::EnablePush(ref val) => val.clone(),
+            &HttpSetting::MaxConcurrentStreams(ref val) => val.clone(),
+            &HttpSetting::InitialWindowSize(ref val) => val.clone(),
+            &HttpSetting::MaxFrameSize(ref val) => val.clone(),
+            &HttpSetting::MaxHeaderListSize(ref val) => val.clone(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::{
         unpack_header,
         pack_header,
+        HttpSetting,
     };
 
     /// Tests that the `unpack_header` function correctly returns the
@@ -171,6 +231,68 @@ mod tests {
                 (1 << 24) - 1, 0, 0, 1 + (1 << 8) + (1 << 16) + (1 << 24)
             );
             assert_eq!(pack_header(&header_components), header);
+        }
+    }
+
+    /// Tests that the `HttpSetting::parse_setting` method correctly creates
+    /// settings from raw bytes.
+    #[test]
+    fn test_setting_deserialize() {
+        {
+            let buf = [0, 1, 0, 0, 1, 0];
+
+            let setting = HttpSetting::parse_setting(&buf).unwrap();
+
+            assert_eq!(setting, HttpSetting::HeaderTableSize(1 << 8));
+        }
+        {
+            let buf = [0, 2, 0, 0, 0, 1];
+
+            let setting = HttpSetting::parse_setting(&buf).unwrap();
+
+            assert_eq!(setting, HttpSetting::EnablePush(1));
+        }
+        {
+            let buf = [0, 3, 0, 0, 0, 0];
+
+            let setting = HttpSetting::parse_setting(&buf).unwrap();
+
+            assert_eq!(setting, HttpSetting::MaxConcurrentStreams(0));
+        }
+        {
+            let buf = [0, 4, 0, 0, 0, 1];
+
+            let setting = HttpSetting::parse_setting(&buf).unwrap();
+
+            assert_eq!(setting, HttpSetting::InitialWindowSize(1));
+        }
+        {
+            let buf = [0, 5, 0, 0, 0, 255];
+
+            let setting = HttpSetting::parse_setting(&buf).unwrap();
+
+            assert_eq!(setting, HttpSetting::MaxFrameSize((1 << 8) - 1));
+        }
+        {
+            let buf = [0, 6, 0, 0, 0, 255];
+
+            let setting = HttpSetting::parse_setting(&buf).unwrap();
+
+            assert_eq!(setting, HttpSetting::MaxHeaderListSize((1 << 8) - 1));
+        }
+        {
+            let buf = [0, 7, 0, 0, 0, 255];
+
+            let setting = HttpSetting::parse_setting(&buf);
+
+            assert!(setting.is_none());
+        }
+        {
+            let buf = [0, 0, 0, 0, 0, 255];
+
+            let setting = HttpSetting::parse_setting(&buf);
+
+            assert!(setting.is_none());
         }
     }
 }
