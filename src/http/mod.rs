922001d55b4d648e972afe7faf9c300250006be6
Marko Lalic <marko.lalic@gmail.com>
2015-02-17 19:33:49 +0100

    Add the Header typedef
    A typedef that refers to a 2-tuple of byte vectors: this will represent
    HTTP/2 headers -- at their lowest level.

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -8,6 +8,9 @@ pub mod connection;
 
 /// An alias for the type that represents the ID of an HTTP/2 stream
 pub type StreamId = u32;
+/// An alias for the type that represents HTTP/2 haders. For now we only alias
+/// the tuple of byte vectors instead of going with a full struct representation.
+pub type Header = (Vec<u8>, Vec<u8>);
 
 /// An enum representing errors that can arise when performing operations
 /// involving an HTTP/2 connection.
