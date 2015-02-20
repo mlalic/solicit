9d9885756a1860d1b70684fa6c40989966e448b0
Marko Lalic <marko.lalic@gmail.com>
2015-02-21 00:52:32 +0100

    Add a Stream trait for representing HTTP/2 streams
    The trait defines which operations need to be defined by a type that should
    be useable as an HTTP/2 stream. By implementing this trait, clients can only
    implement stream-level logic, such as how the received data should be handled,
    instead of tracking which streams exist and what their states are.

--- src/http/session.rs
+++ src/http/session.rs
@@ -22,3 +22,27 @@ pub trait Session {
     /// Notifies the `Session` that a particular stream got closed by the peer.
     fn end_of_stream(&mut self, stream_id: StreamId);
 }
+
+/// A trait representing a single HTTP/2 client stream. An HTTP/2 connection
+/// multiplexes a number of streams.
+///
+/// The trait defines which operations need to be defined by a type that should
+/// be useable as an HTTP/2 stream. By implementing this trait, clients can only
+/// implement stream-level logic, such as how the received data should be handled,
+/// instead of tracking which streams exist and what their states are.
+pub trait Stream {
+    /// Create a new stream with the given ID
+    fn new(stream_id: StreamId) -> Self;
+    /// Handle a new data chunk that has arrived for the stream.
+    fn new_data_chunk(&mut self, data: &[u8]);
+    /// Set headers for a stream. A stream is only allowed to have one set of
+    /// headers.
+    fn set_headers(&mut self, headers: Vec<Header>);
+    /// Close the stream.
+    fn close(&mut self);
+
+    /// Returns the ID of the stream.
+    fn id(&self) -> StreamId;
+    /// Returns whether the stream is closed.
+    fn is_closed(&self) -> bool;
+}
