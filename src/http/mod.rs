de2b51dfc33bb2da643eede5be37ac3f263dd591
Marko Lalic <marko.lalic@gmail.com>
2015-05-08 17:13:46 +0200

    Tests cleanup: Move the root http mod tests to a separate file

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -173,54 +173,6 @@ pub struct Request {
     pub body: Vec<u8>,
 }
 
+
 #[cfg(test)]
-mod tests {
-    use super::{Response, HttpError, HttpScheme};
-
-    /// Tests that the `Response` struct correctly parses a status code from
-    /// its headers list.
-    #[test]
-    fn test_parse_status_code_response() {
-        {
-            // Only status => Ok
-            let resp = Response::new(
-                1,
-                vec![(b":status".to_vec(), b"200".to_vec())],
-                vec![]);
-            assert_eq!(resp.status_code().ok().unwrap(), 200);
-        }
-        {
-            // Extra headers => still works
-            let resp = Response::new(
-                1,
-                vec![(b":status".to_vec(), b"200".to_vec()),
-                     (b"key".to_vec(), b"val".to_vec())],
-                vec![]);
-            assert_eq!(resp.status_code().ok().unwrap(), 200);
-        }
-        {
-            // Status is not the first header => malformed
-            let resp = Response::new(
-                1,
-                vec![(b"key".to_vec(), b"val".to_vec()),
-                     (b":status".to_vec(), b"200".to_vec())],
-                vec![]);
-            assert_eq!(resp.status_code().err().unwrap(),
-                       HttpError::MalformedResponse);
-        }
-        {
-            // No headers at all => Malformed
-            let resp = Response::new(1, vec![], vec![]);
-            assert_eq!(resp.status_code().err().unwrap(),
-                       HttpError::MalformedResponse);
-        }
-    }
-
-    /// Tests that the `HttpScheme` enum returns the correct scheme strings for
-    /// the two variants.
-    #[test]
-    fn test_scheme_string() {
-        assert_eq!(HttpScheme::Http.as_bytes(), b"http");
-        assert_eq!(HttpScheme::Https.as_bytes(), b"https");
-    }
-}
+pub mod tests;
