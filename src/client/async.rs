829139fb2defeedc2455cf9364133ca6d41cb9e2
Joe Wilm <joe@jwilm.com>
2016-03-30 08:57:52 -0700

    Add support for PING frame
    The protocol specifies a few things that could not be handled here due
    to current Solicit APIs.
    
    > If a PING frame is received with a stream identifier field value other
    > than 0x0, the recipient MUST respond with a connection error (Section
    > 5.4.1) of type PROTOCOL_ERROR.
    
    and
    
    > Receipt of a PING frame with a length field value other than 8 MUST be
    > treated as a connection error (Section 5.4.1) of type
    > FRAME_SIZE_ERROR.
    
    When receiving a PING frame without the ACK flag set, the connection
    replies with a PING frame having the ACK flag set. When an ACK is
    received, nothing is done.
    
    In addition to supporting the PING frame of the HTTP/2 protocol, a
    method is exposed on the Async client to support sending PINGs as
    desired by the client. This part of the feature is a bit half-baked as
    the client has no way of getting the PONG. It wasn't clear how this
    API should work. At the very least, it provides a method which will send
    PING frames and return an error immediately if the client isn't capable
    of making requests.

--- src/client/async.rs
+++ src/client/async.rs
@@ -237,6 +237,8 @@ enum WorkItem {
     /// Signals to the service that a client has disconnected. Helps it keep track of whether there
     /// are clients that would expect a response.
     ClientLeft,
+    /// Send a PING frame to the server
+    SendPing,
 }
 
 /// An internal struct encapsulating a service that lets multiple clients
@@ -427,7 +429,11 @@ impl ClientService {
                 debug!("Will queue some request data");
                 try!(self.conn.send_next_data(&mut self.send_handle));
                 Ok(())
-            }
+            },
+            WorkItem::SendPing => {
+                self.send_ping();
+                Ok(())
+            },
             WorkItem::NewClient => {
                 self.client_count += 1;
                 Ok(())
@@ -562,6 +568,11 @@ impl ClientService {
                 self.send_request(async_req);
             }
         }
+    }
+
+    /// Internal helper method to send a PING frame to the server
+    fn send_ping(&mut self) {
+        self.conn.send_ping(&mut self.send_handle).ok().unwrap();
     }
 }
 
@@ -759,5 +770,10 @@ impl Client {
                 body: Vec<u8>)
                 -> Option<Receiver<StaticResponse>> {
         self.request(b"POST", path, headers, Some(body))
+    }
+
+    /// Sends a PING to the server
+    pub fn ping(&self) -> Result<(), &'static str> {
+        self.sender.send(WorkItem::SendPing).map_err(|_| "Client not available")
     }
 }
